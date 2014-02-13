c179bf84df37646cda888293cd5cb670c1e7f845
baskal <baskal.darya@gmail.com>
2014-02-13 13:17:48 +0400

    added project

---
+++ src/main/java/test.java
@@ -0,0 +1,25 @@
+/**
+ * Created by dbaskal on 2/12/14.
+ */
+
+
+import Math_problems.Circle;
+import Math_problems.Set_of_equations;
+import Math_problems.Triangle;
+
+class Math {
+
+    public static void main(String[] args) {
+        //Circle c = new Circle(-5);
+        //c.getPerimeter();
+        //Set_of_equations s = new Set_of_equations();
+        //s.getSolution();
+        //Quadratic q = new Quadratic(10, 2, 1);
+        //q.getSolution();
+        Triangle t = new Triangle(3., 0.1, 5.);
+        t.isRight();
+        //System.out.print(c.getArea());
+
+    }
+
+}
